9d1fbcc83bb75ff4c3e9d063da7ef8975f936c9a
Pearl Kayastha <pearl.gkp@gmail.com>
2026-07-03 21:13:41 +0530

    Screens added

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,5 @@
-import SplashScreen from "./src/screens/splash/SplashScreen";
-
-// export default function App() {
-//   return <SplashScreen />;
-// }
-import OnboardingScreen from "./src/screens/onboarding/OnboardingScreen";
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
-  return <OnboardingScreen />;
+  return <AppNavigator />;
 }
