0dc66148fab5462f204c45c1448f1a67fd3a0aea
Y3s3h <yy6922341@gmail.com>
2026-07-22 08:48:12 +0530

    done

--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 export const socket = io(
-  process.env.NEXT_PUBLIC_SOCKET_URL!,
+  process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:8000",
   {
     transports: ["websocket"],
   }
